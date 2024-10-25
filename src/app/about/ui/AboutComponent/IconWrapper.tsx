ac8498b19a5130115fd4c5106147a493a7085b5e
devmuraCode <muradkusherbaev@gmail.com>
2024-10-25 11:16:55 +0500

    ready dropdown

---
+++ src/app/about/ui/AboutComponent/IconWrapper.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+import styles from "./AboutComponent.module.scss";
+
+interface IconWrapperProps {
+  text: string;
+  children: ReactNode;
+}
+
+const IconWrapper: React.FC<IconWrapperProps> = ({ text, children }) => {
+  return (
+    <div className={styles.icon_wrapper}>
+      <div className={styles.icon}>{children}</div>
+      <p className={styles.text}>{text}</p>
+    </div>
+  );
+};
+
+export default IconWrapper;
