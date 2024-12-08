5ee6af0a221bfbef2cf364ccf6f9c4d4f99f80d8
muradevCode <muradkusherbaev@gmail.com>
2024-12-09 04:17:41 +0500

    w

---
+++ src/app/ourproducts/page.tsx
@@ -0,0 +1,10 @@
+import { ProductListSection } from "./sections/ProductListSection/ProductListSection";
+
+export const OurProducts = () => {
+  return (
+    <div>
+      <ProductListSection />
+    </div>
+  );
+};
+export default OurProducts;
